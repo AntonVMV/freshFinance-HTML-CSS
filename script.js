a288a440371b64ba3dc50b5e3d1b31139153f8e8
AntonVMV <nogdire@gmail.com>
2021-11-27 13:13:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('active');
+        }
+    });
+}
+
+let options = { threshold: [0.3] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+    observer.observe(elm);
+}   
+
+
+const priceCards = document.querySelectorAll('.pricing__card');
+priceCards.forEach(item => {
+    item.addEventListener('mouseenter', classToggle);
+    item.addEventListener('mouseleave', classToggle);
+})
+
+const strategyCards = document.querySelectorAll('.strategy__card');
+strategyCards.forEach(item => {
+    item.addEventListener('mouseenter', classToggle);
+    item.addEventListener('mouseleave', classToggle);
+})
+
+
+
+function classToggle(){
+    this.classList.toggle('active');
+}
